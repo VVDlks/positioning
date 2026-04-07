40f6095b03d80d5dd13972453ab1d63cd027603d
Mariya <maripolevay@gmail.com>
2026-04-07 20:18:19 +0500

    DONE

--- index.js
+++ index.js
@@ -9,22 +9,22 @@
 const modal = document.getElementsByClassName('modalMain')[0];
 const buttonCls = document.getElementsByClassName('closeModal')[0];
 const buttonOpn = document.getElementsByClassName('openModal')[0];
-
-buttonOpn.onclick = () => modal.classList.add('open');
-buttonCls.onclick = () => modal.classList.remove('open');
-
 const fillElement = document.querySelector('.fill');
-let frame = 0;
 const framesCount = 120;
 const frameDuration = 3000 / framesCount;
 
-const interval = setInterval(() => {
-    frame++;
-    const progress = (frame / framesCount * 100);
-    fillElement.style.width = progress + '%';
-    document.querySelector('.text-white').style.clipPath = `polygon(0 0, ${progress}% 0, ${progress}% 100%, 0 100%)`;
-    document.querySelector('.text-black').style.clipPath = `polygon(${progress}% 0, 100% 0, 100% 100%, ${progress}% 100%)`;
-    if (frame >= framesCount) {
-        clearInterval(interval);
-    }
-}, frameDuration);
+buttonOpn.onclick = () => {
+    modal.classList.add('open');
+    let frame = 0;
+    const interval = setInterval(() => {
+        frame++;
+        const progress = (frame / framesCount * 100);
+        fillElement.style.width = progress + '%';
+        document.querySelector('.text-white').style.clipPath = `polygon(0 0, ${progress}% 0, ${progress}% 100%, 0 100%)`;
+        document.querySelector('.text-black').style.clipPath = `polygon(${progress}% 0, 100% 0, 100% 100%, ${progress}% 100%)`;
+        if (frame >= framesCount) {
+            clearInterval(interval);
+        }
+    }, frameDuration);
+};
+buttonCls.onclick = () => modal.classList.remove('open');
