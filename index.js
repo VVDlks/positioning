a2ffa868539519ac5f24716393fd415752985c73
Timofey <ershov.timofei5@gmail.com>
2026-04-07 19:59:35 +0500

    2

--- index.js
+++ index.js
@@ -5,3 +5,10 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const modal = document.getElementsByClassName('modalMain')[0];
+const buttonCls = document.getElementsByClassName('closeModal')[0];
+const buttonOpn = document.getElementsByClassName('openModal')[0];
+
+buttonOpn.onclick = () => modal.classList.add('open');
+buttonCls.onclick = () => modal.classList.remove('open');
